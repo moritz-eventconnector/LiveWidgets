fe6bc92de841d57706296f4ab15e6d6c513fae07
EventConnector <moritz@eventconnector.ch>
2026-01-26 22:09:48 +0100

    Ignore env backups in update guard

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import HomePage from '@/components/HomePage';
 
-export const dynamic = 'force-static';
-export const revalidate = false;
-
 export default function Home() {
   return <HomePage />;
 }
--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,4 +1,4 @@
-const currentYear = new Date().getFullYear();
+const currentYear = 2024;
 
 const features = [
   {
